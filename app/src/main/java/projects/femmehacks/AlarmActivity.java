dc95982879a307876ec297cf35d0e81179471ac1
jouwenlu <jouwenlu@seas.upenn.edu>
2021-02-07 05:53:59 +0800

    commit -m "added time picker" commit -m "added ontime listener"

--- app/src/main/java/projects/femmehacks/AlarmActivity.java
+++ app/src/main/java/projects/femmehacks/AlarmActivity.java
@@ -1,8 +1,10 @@
 package projects.femmehacks;
-//
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,69 +14,31 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+import android.widget.TimePicker;
+
+
 
 public class AlarmActivity extends AppCompatActivity {
-    private String hour;
-    private String minute;
+    private TimePicker timePicker;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
 
-        /*
-        SeekBar hourBar= findViewById(R.id.hourSlider);
-        // perform seek bar change listener event used for getting the progress value
-        hourBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progressChangedValue = 0;
-            String hour;
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progressChangedValue = progress;
-                TextView textView = (TextView) findViewById(R.id.hour);
-                String hourTime;
-                if (progress < 10) {
-                    hourTime = Integer.toString(progress);
-                    hourTime = "0" + hourTime;
-                    textView.setText(hourTime);
-                } else {
-                    hourTime = Integer.toString(progress);
-                    textView.setText(hourTime);
-                }
-                hour = hourTime;
-            }
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-            public void onStopTrackingTouch(SeekBar seekBar) {
+        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+        this.timePicker = timePicker;
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute){
+                String date = hourOfDay + ":" + minute;
+                TextView textView = findViewById(R.id.timeDisplay);
+                textView.setText(date);
             }
         });
 
-        SeekBar minuteBar= findViewById(R.id.minuteSlider);
-        // perform seek bar change listener event used for getting the progress value
-        minuteBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progressChangedValue = 0;
-
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progressChangedValue = progress;
-                TextView textView = (TextView) findViewById(R.id.minute);
-                String minuteTime;
-                if (progress < 10) {
-                    minuteTime = Integer.toString(progress);
-                    minuteTime = "0" + minuteTime;
-                    textView.setText(minuteTime);
-                } else {
-                    minuteTime = Integer.toString(progress);
-                    textView.setText(minuteTime);
-                }
-
-            }
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-
-
-         */
         Spinner spinner = (Spinner) findViewById(R.id.goal_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -98,7 +62,11 @@ public class AlarmActivity extends AppCompatActivity {
     }
 
     public void setAlarm(View view) {
-
+        TextView textView = findViewById(R.id.timeDisplay);
+        CharSequence date = textView.getText();
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("time", date);
+        setResult(AlarmActivity.RESULT_OK,returnIntent);
         finish();
     }
 
--- app/src/main/java/projects/femmehacks/MainActivity.java
+++ app/src/main/java/projects/femmehacks/MainActivity.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +18,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void toDiffPage(View view) {
         Intent myIntent = new Intent(view.getContext(), AlarmActivity.class);
-        startActivity(myIntent);
+        startActivityForResult(myIntent, 0);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0) {
+            if(resultCode == AlarmActivity.RESULT_OK){
+                CharSequence time = data.getCharSequenceExtra("time");
+                TextView textView = findViewById(R.id.timeDisplay);
+                textView.setText(time);
+
+
+            }
+            if (resultCode == AlarmActivity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }//onActivityResult
 
 
 
